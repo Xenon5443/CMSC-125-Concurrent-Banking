e46a710b32ee1fdc107fb59a177f9d888d59fc69
Xenon5443 <mtmisola@up.edu.ph>
2026-04-10 21:25:40 +0800

    fix: Replaced buffer pools structs with its header file

--- bankdb/src/buffer_pool.c
+++ bankdb/src/buffer_pool.c
@@ -1,28 +1,11 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <semaphore.h>
+
+#include "buffer_pool.h"
 #include "bank.h"
 
 #define BUFFER_POOL_SIZE 5
-
-typedef struct {
-    int account_id;
-    Account* data;
-    bool in_use;
-} BufferSlot;
-
-typedef struct {
-    BufferSlot slots[BUFFER_POOL_SIZE];
-    sem_t empty_slots;
-    sem_t full_slots;
-    pthread_mutex_t pool_lock;
-} BufferPool;
-
-void init_buffer_pool(BufferPool* pool) {
-    sem_init(&pool->empty_slots, 0, BUFFER_POOL_SIZE);
-    sem_init(&pool->full_slots, 0, 0);
-    pthread_mutex_init(&pool->pool_lock, NULL);
-}
 
 // Load account into buffer pool (producer)
 void load_account(BufferPool* pool, int account_id) {
