418b0f22d069587be719089cead17cab470e8e73
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-05-08 11:04:40 +0800

    feat/initial main code

--- bankdb/src/main.c
+++ bankdb/src/main.c
@@ -1,28 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "bank.h"
 #include "transaction.h"
 #include "utils.h"
+#include "timer.h"
+#include "simulation.h"
+
+
+int TICK_INTERVAL_MS = 100; 
+bool simulation_running = true;
 
 int main(int argc, char *argv[]) {
     //declared struct for convenience of passing parameters and to avoid working with pointers
     Config config; 
 
     char accounts_full_path[512];
-    char* folder = "../tests/";
     char trace_full_path[512];
     
     // Pass by pointer so the function can modify the 'config' variable
     parse_cli_arguments(argc, argv, &config);
 
-    // Concatenation to obtain complete path
-    snprintf(accounts_full_path, sizeof(accounts_full_path), "%s%s", folder,  config.accounts_file);
-    snprintf(trace_full_path, sizeof(trace_full_path), "%s%s", folder, config.trace_file);
-
     // parse account and trace
-    Bank my_bank = parse_accounts(accounts_full_path);
-    Transaction* my_transactions = parse_transactions(trace_full_path);
+    parse_accounts(config.accounts_file);
+
+    int num_tx = 0;
+    Transaction* my_transactions = parse_transactions(config.trace_file, &num_tx);
+
+
+    // 2. Initialize Synchronization Tools
+    pthread_mutex_init(&tick_lock, NULL);
+    pthread_cond_init(&tick_changed, NULL);
+    
+    // 3. Launch the Timer in its OWN thread
+    pthread_t timer_tid;
+    pthread_create(&timer_tid, NULL, timer_thread, NULL); 
+
+    // 4. Main continues immediately to the next line
+    // It can now start transaction threads, which will call wait_until_tick()
+    run_simulation(my_transactions, num_tx);
+
+    // 5. Cleanup
+    simulation_running = false; // Tell the timer loop to stop
+    pthread_join(timer_tid, NULL); // Wait for timer thread to finish
+
+
 
     free(my_transactions);
 
