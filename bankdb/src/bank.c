96c6e64c13b53034cd8f42fb920c1683ba9a925d
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-05-11 05:12:28 +0800

    feat: add commented load and unload in bank,c

--- bankdb/src/bank.c
+++ bankdb/src/bank.c
@@ -1,38 +1,51 @@
 #include <stdbool.h>
 #include "bank.h"
+#include "lock_mgr.h"
+#include "buffer_pool.h"
 
-bank = {0};
+Bank bank;
 
 int get_balance(int account_id) {
+    
+    // load_account(&pool, account_id);
     Account* acc = &bank.accounts[account_id];
-    
     pthread_rwlock_rdlock(&acc->lock);
-    int balance = acc->balance_centavos;
-    pthread_rwlock_unlock(&acc->lock);
     
+    int balance = acc->balance_centavos;
+
+    pthread_rwlock_unlock(&acc->lock);
+    // unload_account(&pool, account_id);
     return balance;
 }
 
 void deposit(int account_id, int amount_centavos) {
+
+    // load_account(&pool, account_id);
     Account* acc = &bank.accounts[account_id];
     
     pthread_rwlock_wrlock(&acc->lock);
     acc->balance_centavos += amount_centavos;
     pthread_rwlock_unlock(&acc->lock);
+    // unload_account(&pool, account_id);
 }
 
 bool withdraw(int account_id, int amount_centavos) {
+
+    // load_account(&pool, account_id);
     Account* acc = &bank.accounts[account_id];
     
     pthread_rwlock_wrlock(&acc->lock);
     
     if (acc->balance_centavos < amount_centavos) {
         pthread_rwlock_unlock(&acc->lock);
+        // unload_account(&pool, account_id);
         return false;  // Insufficient funds
     }
     
     acc->balance_centavos -= amount_centavos;
     pthread_rwlock_unlock(&acc->lock);
+
+    // unload_account(&pool, account_id);
     return true;
 }
 
@@ -41,26 +54,32 @@ bool transfer(int from_id, int to_id, int amount_centavos) {
     // Acquire locks in consistent order to prevent deadlock
     int first = (from_id < to_id) ? from_id : to_id;
     int second = (from_id < to_id) ? to_id : from_id;
+
+    // load_account(&pool, first);
+    // load_account(&pool, second);
     
     Account* acc_first = &bank.accounts[first];
     Account* acc_second = &bank.accounts[second];
-    
-    pthread_rwlock_wrlock(&acc_first->lock);
-    pthread_rwlock_wrlock(&acc_second->lock);
+
+    lock_rw(acc_first, acc_second);
     
     // Check sufficient funds
     Account* from_acc = &bank.accounts[from_id];
     if (from_acc->balance_centavos < amount_centavos) {
-        pthread_rwlock_unlock(&acc_second->lock);
-        pthread_rwlock_unlock(&acc_first->lock);
+        unlock_rw(acc_first, acc_second);
+
+        // unload_account(&pool, first);
+        // unload_account(&pool, second);
         return false;
     }
     
     // Perform transfer
     bank.accounts[from_id].balance_centavos -= amount_centavos;
     bank.accounts[to_id].balance_centavos += amount_centavos;
-    
-    pthread_rwlock_unlock(&acc_second->lock);
-    pthread_rwlock_unlock(&acc_first->lock);
+
+    unlock_rw(acc_first, acc_second);
+
+    // unload_account(&pool, first);
+    // unload_account(&pool, second);
     return true;
 }
