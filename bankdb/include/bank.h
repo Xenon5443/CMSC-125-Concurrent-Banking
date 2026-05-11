0d70e87606c93928162d03f6fbc9c78ef897cac6
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-05-11 21:42:45 +0800

    fix: prototypes in bank.h

--- bankdb/include/bank.h
+++ bankdb/include/bank.h
@@ -23,6 +23,6 @@ extern Bank bank;
 int get_balance(int account_id);
 void deposit(int account_id, int amount_centavos);
 bool withdraw(int account_id, int amount_centavos);
-bool transfer(int from_id, int to_id, int amount_centavos);
+bool transfer(int from_id, int to_id, int amount_centavos, int tx_id);
 
 #endif
