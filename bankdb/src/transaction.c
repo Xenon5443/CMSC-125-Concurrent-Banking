34c5decfb55dcefb4a301eb79bf85d262b50363a
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-05-11 21:40:50 +0800

    fix: loading and unloading in transaction.c
    commented loading and unloading due to to encountering thread sanitizer warning

--- bankdb/src/transaction.c
+++ bankdb/src/transaction.c
@@ -18,27 +18,33 @@ void* execute_transaction(void* arg) {
     for (int i = 0; i < tx->num_ops; i++) {
         Operation* op = &tx->ops[i];
 
-        int first;
-        int second;
+        //If i uncomment this, a threadSanitizer warning occurs 
 
-        if(op->type == OP_TRANSFER){
-            first = (op->account_id < op->target_account) ? op->account_id : op->target_account;
-            second = (op->account_id < op->target_account) ? op->target_account : op->account_id;
+        // int first;
+        // int second;
 
-            load_account(&pool, first);
-            load_account(&pool, second);
-        }else{
-            load_account(&pool, op->account_id);
-        }
+        // if(op->type == OP_TRANSFER){
+        //     first = (op->account_id < op->target_account) ? op->account_id : op->target_account;
+        //     second = (op->account_id < op->target_account) ? op->target_account : op->account_id;
+
+        //     load_account(&pool, first);
+        //     load_account(&pool, second);
+        // }else{
+        //     load_account(&pool, op->account_id);
+        // }
         
         int tick_before = global_tick;
         
         switch (op->type) {
             case OP_DEPOSIT:
+                printf("  T%d started: DEPOSIT account %d amount PHP %.2f\n", 
+                tx->tx_id, op->account_id, (float)op->amount_centavos / 100.0);
                 deposit(op->account_id, op->amount_centavos);
                 break;
                 
             case OP_WITHDRAW:
+                printf("  T%d started: WITHDRAW account %d amount PHP %.2f\n", 
+                tx->tx_id, op->account_id, (float)op->amount_centavos / 100.0);
                 if (!withdraw(op->account_id, op->amount_centavos)) {
                     // Insufficient funds - abort transaction
                     tx->status = TX_ABORTED;
@@ -47,33 +53,47 @@ void* execute_transaction(void* arg) {
                 break;
                 
             case OP_TRANSFER:
+                printf("  T%d started: TRANSFER from %d to %d amount PHP %.2f\n", 
+                tx->tx_id, op->account_id,op->target_account, (float)op->amount_centavos / 100.0);
                 if (!transfer(op->account_id, op->target_account,
-                              op->amount_centavos)) {
+                              op->amount_centavos, tx->tx_id)) {
                     tx->status = TX_ABORTED;
                     return NULL;
                 }
                 break;
                 
             case OP_BALANCE:
+                printf("  T%d started: BALANCE account %d\n", 
+                tx->tx_id, op->account_id);
                 int balance = get_balance(op->account_id);
-                printf("T%d: Account %d balance = PHP %d.%02d\n", 
+                printf("  T%d: Account %d balance = PHP %d.%02d\n", 
                        tx->tx_id, op->account_id, 
                        balance / 100, balance % 100);
                 break;
         }
 
-        if(op->type == OP_TRANSFER){
+        // if(op->type == OP_TRANSFER){
 
-            unload_account(&pool, first);
-            unload_account(&pool, second);
-        }else{
-            unload_account(&pool, op->account_id);
-        }
+        //     unload_account(&pool, first);
+        //     unload_account(&pool, second);
+        // }else{
+        //     unload_account(&pool, op->account_id);
+        // }
         
         tx->wait_ticks += (global_tick - tick_before);
+
+        // At the very end of the function
+        printf("  T%d completed: %s successful\n", 
+            tx->tx_id, 
+            (op->type == 0) ? "DEPOSIT" : 
+            (op->type == 1) ? "WITHDRAW" : 
+            (op->type == 2) ? "TRANSFER" : 
+            (op->type == 3) ? "BALANCE"  : "UNKNOWN" // Added the fallback here
+        );
     }
     
     tx->actual_end = global_tick;
     tx->status = TX_COMMITTED;
+
     return NULL;
 }
