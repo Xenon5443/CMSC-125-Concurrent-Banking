942d2a875f8b30cb9e6b4c64d1c4485618203149
YoungFEK <197771340+YoungFEK@users.noreply.github.com>
2026-05-21 22:40:40 +0800

    refactor: change declaration type of simulation_running to atomic_bool

--- bankdb/include/timer.h
+++ bankdb/include/timer.h
@@ -1,16 +1,21 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <stdatomic.h>
 #include <stdbool.h>
 #include <pthread.h> // Necessary for mutex and cond types
 
 extern volatile int global_tick;
 extern int TICK_INTERVAL_MS;
-extern volatile bool simulation_running;
+// extern volatile bool simulation_running;
+extern atomic_bool simulation_running;
 
 // Add these two lines:
 extern pthread_mutex_t tick_lock;
 extern pthread_cond_t tick_changed;
+
+// Add this function declaration
+int read_global_tick(void);
 
 void* timer_thread();
 void wait_until_tick(int target_tick);
